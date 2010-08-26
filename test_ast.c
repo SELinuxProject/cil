31fb9c2a2e4f91fbe046c90e9f53a21c06924bcd
Jeremy Solt <jsolt@tresys.com>
2010-08-26 12:08:03 -0400

    Fix yy_scan_buffer issue Fix some spacing issues

--- test_ast.c
+++ test_ast.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #include "cil_lexer.h"
 #include "cil.h"
@@ -14,9 +15,9 @@
 int main(int argc, char *argv[])
 {
 	struct stat filedata;
-        uint32_t file_size;
-        char *buffer;
-        FILE *file;
+	uint32_t file_size;
+	char *buffer;
+	FILE *file;
 	
 	struct cil_tree *parse_root;
 	cil_tree_init(&parse_root);
@@ -24,23 +25,24 @@ int main(int argc, char *argv[])
 	struct cil_db *db;
 	cil_db_init(&db);
 
-        if (argc > 1) {
-                file = fopen(argv[1], "r");
-                if (!file) {
-                        fprintf(stderr, "Could not open file\n");
-                        return SEPOL_ERR;
-                }
+	if (argc > 1) {
+		file = fopen(argv[1], "r");
+		if (!file) {
+			 fprintf(stderr, "Could not open file\n");
+			 return SEPOL_ERR;
+		}
 		if (stat(argv[1], &filedata) == -1) {
 			printf("Could not stat file\n");
 			return SEPOL_ERR;
 		}
 		file_size = filedata.st_size;	
 
-                buffer = malloc(file_size + 1);
-                fread(buffer, file_size, 1, file); 
-                fclose(file);           
+		buffer = malloc(file_size + 2);
+		fread(buffer, file_size, 1, file);
+		memset(buffer+file_size, 0, 2);
+		fclose(file);
 
-		cil_parser(buffer, file_size, &parse_root);
+		cil_parser(buffer, file_size + 2, &parse_root);
 
 		cil_tree_print(parse_root->root, 0);
 	
@@ -53,8 +55,8 @@ int main(int argc, char *argv[])
 		printf("id: %d\n", search->datum.value);
 		printf("first child is of type: %d\n", search->self->cl_head->flavor);		
 
-        }
+	}
 
-        return SEPOL_OK;
+	return SEPOL_OK;
 }
 
--- test_parser.c
+++ test_parser.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/stat.h>
+#include <string.h>
 
 #include "cil_lexer.h"
 #include "cil_tree.h"
@@ -12,31 +13,31 @@ int main(int argc, char *argv[])
 {
 	struct stat filedata;
 	struct cil_tree *tree;
-        uint32_t file_size;
-        char *buffer;
-        FILE *file;
+	uint32_t file_size;
+	char *buffer;
+	FILE *file;
 
-        if (argc > 1) {
-                file = fopen(argv[1], "r");
-                if (!file) {
-                        fprintf(stderr, "Could not open file\n");
-                        return SEPOL_ERR;
-                }
+	if (argc > 1) {
+		file = fopen(argv[1], "r");
+		if (!file) {
+			fprintf(stderr, "Could not open file\n");
+			return SEPOL_ERR;
+		}
 		if (stat(argv[1], &filedata) == -1) {
 			printf("Could not stat file\n");
 			return SEPOL_ERR;
 		}	
 		file_size = filedata.st_size;
 	
-                buffer = malloc(file_size + 2);
-                fread(buffer, file_size, 1, file); 
-                memset(buffer+file_size, 0, 2);
-                fclose(file);           
+		buffer = malloc(file_size + 2);
+		fread(buffer, file_size, 1, file);
+		memset(buffer+file_size, 0, 2);
+		fclose(file);
 
 		cil_parser(buffer, file_size+2, &tree);
 		cil_tree_print(tree->root, 0);
-        }
+	}
 
-        return SEPOL_OK;
+	return SEPOL_OK;
 }
 
