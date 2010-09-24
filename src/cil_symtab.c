3f834f40dd96ae0189382c9b022e7dce7188f419
Jeremy Solt <jsolt@tresys.com>
2010-09-24 11:00:00 -0400

    strdup key in symtab insert

--- src/cil_symtab.c
+++ src/cil_symtab.c
@@ -1,13 +1,16 @@
+#include <stdlib.h>
 #include <sepol/errcodes.h>
 #include <sepol/policydb/hashtab.h>
+#include <string.h>
+#include "cil_tree.h"
 #include "cil_symtab.h"
 
 int cil_symtab_insert(symtab_t *symtab, hashtab_key_t key, cil_symtab_datum_t *datum, struct cil_tree_node *node)
 {
-	// TODO CDS newkey = strdup(key)
-	int rc = hashtab_insert(symtab->table, key, (hashtab_datum_t)datum);
+	char *newkey = strdup(key);
+	int rc = hashtab_insert(symtab->table, newkey, (hashtab_datum_t)datum);
 	if (rc) {
-		// TODO CDS don't forget to free - free(newkey)
+		free(newkey);
 		return rc;
 	}
 	else {
