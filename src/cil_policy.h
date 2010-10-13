30c0150efd8f303b1783b8872d79b11e408c1678
Jason Dana <jdana@tresys.com>
2010-10-13 13:43:37 -0400

    Added ordering to policy.conf creation pass. Added more error handling to policy.conf creation pass.

--- src/cil_policy.h
+++ src/cil_policy.h
@@ -4,6 +4,7 @@
 #include "cil_tree.h"
 #include "cil.h"
 
+int cil_combine_policy(FILE **, FILE *);
 int cil_name_to_policy(FILE *, struct cil_tree_node *); 
 int cil_gen_policy(struct cil_tree_node *);
 
