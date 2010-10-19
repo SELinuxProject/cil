123b9a0831e9e2c5637e9b88682a6a54d1c1934c
Jason Dana <jdana@tresys.com>
2010-10-19 09:40:19 -0400

    Add positive testing for cil_gen_user()

--- testing/unit/CilTest.c
+++ testing/unit/CilTest.c
@@ -1286,6 +1286,28 @@ void test_cil_gen_avrule_notlist(CuTest *tc) {
 	CuAssertPtrNotNull(tc, ((struct cil_avrule*)test_ast_node->data)->perms_str);
 }
 
+void test_cil_gen_user(CuTest *tc) {
+	char *line[] = {"(", "user", "sysadm", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	int rc = cil_gen_user(test_db, tree->root->cl_head->cl_head, test_ast_node);
+	CuAssertIntEquals(tc, SEPOL_OK, rc);
+	CuAssertIntEquals(tc, CIL_USER, test_ast_node->flavor);
+	CuAssertPtrNotNull(tc, test_ast_node->data);
+	CuAssertPtrEquals(tc, test_ast_node, ((struct cil_symtab_datum*)test_ast_node->data)->node);
+	CuAssertStrEquals(tc, tree->root->cl_head->cl_head->next->data, ((struct cil_symtab_datum*)test_ast_node->data)->name);
+}
+
 void test_cil_build_ast(CuTest *tc) {
 	char *line[] = {"(", "test", "\"qstring\"", ")", ";comment", NULL};
 
@@ -1875,6 +1897,7 @@ CuSuite* CilTreeGetSuite() {
 	SUITE_ADD_TEST(suite, test_cil_gen_avrule);
 //	Is a perm list with a single value, not surrounded by parenthesis, valid?
 	SUITE_ADD_TEST(suite, test_cil_gen_avrule_notlist);
+	SUITE_ADD_TEST(suite, test_cil_gen_user);
 	SUITE_ADD_TEST(suite, test_cil_build_ast);
 	SUITE_ADD_TEST(suite, test_cil_build_ast_dbnull_neg);
 	SUITE_ADD_TEST(suite, test_cil_build_ast_astnull_neg);
