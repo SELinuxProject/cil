a66c92f7cda9a6ba8cf832358f03fd4aafbb3e88
Jeremy Solt <jsolt@tresys.com>
2010-11-08 15:05:08 -0500

    remove really old build scripts

--- src/build-ast.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-flex -o cil_lexer.c cil_lexer.l
-gcc -Wall -o ast test_ast.c cil.c cil_tree.c cil_ast.c cil_parser.c cil_lexer.c cil_symtab.c /usr/lib64/libsepol.a -lfl
--- src/build-parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-flex -o cil_lexer.c cil_lexer.l
-gcc -Wall test_parser.c cil.c cil_tree.c cil_ast.c cil_parser.c cil_lexer.c cil_symtab.c /usr/lib64/libsepol.a -lfl -o parser 
