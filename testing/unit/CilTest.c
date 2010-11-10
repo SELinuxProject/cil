a6968a9a862fea7e4e16113a90f25d34071e5088
Jeremy Solt <jsolt@tresys.com>
2010-11-10 13:03:38 -0500

    unit tests for type rule and typetransition

--- testing/unit/CilTest.c
+++ testing/unit/CilTest.c
@@ -1529,6 +1529,149 @@ void test_cil_gen_avrule_notlist_neg(CuTest *tc) {
 	CuAssertIntEquals(tc, SEPOL_ERR, rc);
 }
 
+void test_cil_gen_type_rule_transition(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_OK, rc);
+	CuAssertStrEquals(tc, ((struct cil_type_rule*)test_ast_node->data)->src_str, tree->root->cl_head->cl_head->next->data);
+	CuAssertStrEquals(tc, ((struct cil_type_rule*)test_ast_node->data)->tgt_str, tree->root->cl_head->cl_head->next->next->data);
+	CuAssertStrEquals(tc, ((struct cil_type_rule*)test_ast_node->data)->obj_str, tree->root->cl_head->cl_head->next->next->next->data);
+	CuAssertStrEquals(tc, ((struct cil_type_rule*)test_ast_node->data)->result_str, tree->root->cl_head->cl_head->next->next->next->next->data);
+	CuAssertIntEquals(tc, ((struct cil_type_rule*)test_ast_node->data)->rule_kind, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, test_ast_node->flavor, CIL_TYPE_RULE);
+}
+
+void test_cil_gen_type_rule_transition_currnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	int rc = cil_gen_type_rule(NULL, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc); 
+}
+
+void test_cil_gen_type_rule_transition_astnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node = NULL;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}	
+
+void test_cil_gen_type_rule_transition_srcnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	tree->root->cl_head->cl_head->next = NULL;
+	
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_gen_type_rule_transition_tgtnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	tree->root->cl_head->cl_head->next->next = NULL;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_gen_type_rule_transition_objnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	tree->root->cl_head->cl_head->next->next->next = NULL;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_gen_type_rule_transition_resultnull_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	tree->root->cl_head->cl_head->next->next->next->next = NULL;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_gen_type_rule_transition_extra_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", "extra", ")", NULL};
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_tree_node *test_ast_node;
+	cil_tree_node_init(&test_ast_node);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	test_ast_node->parent = test_db->ast->root;
+	test_ast_node->line = 1;
+
+	int rc = cil_gen_type_rule(tree->root->cl_head->cl_head, test_ast_node, CIL_TYPE_TRANSITION);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
 void test_cil_gen_user(CuTest *tc) {
 	char *line[] = {"(", "user", "sysadm", ")", NULL};
 	struct cil_tree *tree;
@@ -1916,6 +2059,32 @@ void test_cil_build_ast_avrule_neg(CuTest *tc) {
 	cil_db_init(&test_db);
 
 	int rc = cil_build_ast(test_db, test_tree->root, test_db->ast->root);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_build_ast_type_rule_transition(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);	
+	
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	int rc = cil_build_ast(test_db, tree->root, test_db->ast->root);
+	CuAssertIntEquals(tc, SEPOL_OK, rc);
+}
+
+void test_cil_build_ast_type_rule_transition_neg(CuTest *tc) {
+	char *line[] = {"(", "typetransition", "foo", "bar", "file", "foobar", "extra",  ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);	
+	
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	int rc = cil_build_ast(test_db, tree->root, test_db->ast->root);
 	CuAssertIntEquals(tc, SEPOL_ERR, rc);
 }
 
@@ -2310,6 +2479,138 @@ void test_cil_resolve_ast_avrule_datum_null_neg(CuTest *tc) {
 	CuAssertIntEquals(tc, SEPOL_ERR, rc);
 }
 
+void test_cil_resolve_type_rule(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "type", "bar", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "type", "foobar", ")", 
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	int rc = cil_resolve_type_rule(test_db, test_db->ast->root->cl_head->next->next->next->next);
+	CuAssertIntEquals(tc, SEPOL_OK, rc);
+}
+
+void test_cil_resolve_type_rule_srcdecl_neg(CuTest *tc) {
+	char *line[] = {"(", "type", "bar", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "type", "foobar", ")", 
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	int rc = cil_resolve_type_rule(test_db, test_db->ast->root->cl_head->next->next->next);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_resolve_type_rule_tgtdecl_neg(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "type", "foobar", ")", 
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	int rc = cil_resolve_type_rule(test_db, test_db->ast->root->cl_head->next->next->next);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_resolve_type_rule_objdecl_neg(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "type", "bar", ")",
+			"(", "type", "foobar", ")", 
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	int rc = cil_resolve_type_rule(test_db, test_db->ast->root->cl_head->next->next->next);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_resolve_type_rule_resultdecl_neg(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "type", "bar", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	int rc = cil_resolve_type_rule(test_db, test_db->ast->root->cl_head->next->next->next);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
+void test_cil_resolve_ast_type_rule(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "type", "bar", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "type", "foobar", ")",
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	cil_resolve_ast(test_db, test_db->ast->root, 1);
+
+	int rc = cil_resolve_ast(test_db, test_db->ast->root, 3);
+	CuAssertIntEquals(tc, SEPOL_OK, rc);
+}
+
+void test_cil_resolve_ast_type_rule_neg(CuTest *tc) {
+	char *line[] = {"(", "type", "foo", ")",
+			"(", "class", "file", "(", "write", ")", ")",
+			"(", "type", "foobar", ")",
+			"(", "typetransition", "foo", "bar", "file", "foobar", ")", NULL};
+	
+	struct cil_tree *tree;
+	gen_test_tree(&tree, line);
+
+	struct cil_db *test_db;
+	cil_db_init(&test_db);
+
+	cil_build_ast(test_db, tree->root, test_db->ast->root);
+
+	cil_resolve_ast(test_db, test_db->ast->root, 1);
+
+	int rc = cil_resolve_ast(test_db, test_db->ast->root, 3);
+	CuAssertIntEquals(tc, SEPOL_ERR, rc);
+}
+
 CuSuite* CilTreeGetSuite() {
 	CuSuite* suite = CuSuiteNew();
 	SUITE_ADD_TEST(suite, test_cil_tree_node_init);
@@ -2381,6 +2682,14 @@ CuSuite* CilTreeGetSuite() {
 	SUITE_ADD_TEST(suite, test_cil_gen_bool_notbool_neg);
 	SUITE_ADD_TEST(suite, test_cil_gen_avrule);
 	SUITE_ADD_TEST(suite, test_cil_gen_avrule_notlist_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_currnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_astnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_srcnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_tgtnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_objnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_resultnull_neg);
+	SUITE_ADD_TEST(suite, test_cil_gen_type_rule_transition_extra_neg);
 	SUITE_ADD_TEST(suite, test_cil_gen_user);
 	SUITE_ADD_TEST(suite, test_cil_gen_user_nouser_neg);
 	SUITE_ADD_TEST(suite, test_cil_gen_user_xsinfo_neg);
@@ -2412,6 +2721,8 @@ CuSuite* CilTreeGetSuite() {
 	SUITE_ADD_TEST(suite, test_cil_build_ast_bool_neg);
 	SUITE_ADD_TEST(suite, test_cil_build_ast_avrule);
 	SUITE_ADD_TEST(suite, test_cil_build_ast_avrule_neg);
+	SUITE_ADD_TEST(suite, test_cil_build_ast_type_rule_transition);
+	SUITE_ADD_TEST(suite, test_cil_build_ast_type_rule_transition_neg);
 	SUITE_ADD_TEST(suite, test_cil_resolve_name);
 	SUITE_ADD_TEST(suite, test_cil_resolve_name_invalid_type_neg);
 	SUITE_ADD_TEST(suite, test_cil_resolve_typealias);
@@ -2440,6 +2751,13 @@ CuSuite* CilTreeGetSuite() {
 	SUITE_ADD_TEST(suite, test_cil_resolve_ast_avrule_class_nores_neg);
 	SUITE_ADD_TEST(suite, test_cil_resolve_ast_avrule_datum_null_neg);
 	SUITE_ADD_TEST(suite, test_cil_resolve_ast_roleallow);
+	SUITE_ADD_TEST(suite, test_cil_resolve_type_rule);
+	SUITE_ADD_TEST(suite, test_cil_resolve_type_rule_srcdecl_neg);
+	SUITE_ADD_TEST(suite, test_cil_resolve_type_rule_tgtdecl_neg);
+	SUITE_ADD_TEST(suite, test_cil_resolve_type_rule_objdecl_neg);
+	SUITE_ADD_TEST(suite, test_cil_resolve_type_rule_resultdecl_neg);
+	SUITE_ADD_TEST(suite, test_cil_resolve_ast_type_rule);
+	SUITE_ADD_TEST(suite, test_cil_resolve_ast_type_rule_neg);
 
 	return suite;
 }
