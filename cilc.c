547f9a61987c291f7a3aad4affdca22cdb0df73c
Steve Lawrence <slawrence@tresys.com>
2011-06-08 14:36:05 -0400

    Add license information

--- cilc.c
+++ cilc.c
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2011 Tresys Technology, LLC
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -65,10 +83,10 @@ int main(int argc, char *argv[])
 			printf("Failed to parse CIL policy, exiting\n");
 			return SEPOL_ERR;
 		}
-	}
 #ifdef DEBUG
 	cil_tree_print(parse_tree->root, 0);
 #endif
+	}
 
 	printf("Building AST from Parse Tree...\n");
 	if (cil_build_ast(db, parse_tree->root, db->ast->root)) {
