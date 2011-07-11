bb5420767864269b671cf97f984d67174c42c6b2
Jason Dana <jdana@tresys.com>
2011-07-11 10:38:08 -0700

    cleanup cil_binary.c

--- src/cil_binary.c
+++ src/cil_binary.c
@@ -46,35 +46,39 @@ int cil_common_to_policydb(policydb_t *pdb, struct cil_tree_node *node)
 {
 	int rc = SEPOL_ERR;
 	uint32_t value = 0;
-	char *key;
+	char *key = NULL;
 	struct cil_common *cil_common = node->data;
 	struct cil_tree_node *cil_perm = node->cl_head;
 	common_datum_t *sepol_common = cil_malloc(sizeof(*sepol_common));
 	memset(sepol_common, 0, sizeof(common_datum_t));
+
 	key = cil_strdup(cil_common->datum.name);
 	rc = symtab_insert(pdb, SYM_COMMONS, key, sepol_common, SCOPE_DECL, 0, &value);
 	if (rc != SEPOL_OK) {
 		goto common_to_binary_out;
 	}
 	sepol_common->s.value = value;
+
 	rc = symtab_init(&sepol_common->permissions, PERM_SYMTAB_SIZE);
 	if (rc != SEPOL_OK) {
 		goto common_to_binary_out;
 	}
+
 	while (cil_perm != NULL) {
 		struct cil_perm *curr = cil_perm->data;
 		perm_datum_t *sepol_perm = cil_malloc(sizeof(*sepol_perm));
 		memset(sepol_perm, 0, sizeof(perm_datum_t));
-		sepol_perm->s.value = sepol_common->permissions.nprim + 1;
-		rc = hashtab_insert(sepol_common->permissions.table,
-					curr->datum.name,
-					sepol_perm);
+
+		key = cil_strdup(curr->datum.name);
+		rc = hashtab_insert(sepol_common->permissions.table, key, sepol_perm);
 		if (rc != SEPOL_OK) {
 			goto common_to_binary_out;
 		}
+		sepol_perm->s.value = sepol_common->permissions.nprim + 1;
 		sepol_common->permissions.nprim++;
 		cil_perm = cil_perm->next;
 	}
+
 	return SEPOL_OK;
 
 common_to_binary_out:
@@ -85,34 +89,40 @@ int cil_class_to_policydb(policydb_t *pdb, struct cil_tree_node *node)
 {
 	int rc = SEPOL_ERR;
 	uint32_t value = 0;
-	char *key;
+	char *key = NULL;
 	struct cil_class *cil_class = node->data;
 	struct cil_tree_node *cil_perm = node->cl_head;
 	class_datum_t *sepol_class = cil_malloc(sizeof(*sepol_class));
 	memset(sepol_class, 0, sizeof(class_datum_t));
+
 	key = cil_strdup(cil_class->datum.name);
 	rc = symtab_insert(pdb, SYM_CLASSES, key, sepol_class, SCOPE_DECL, 0, &value);
 	if (rc != SEPOL_OK) {
 		goto class_to_policydb_out;
 	}
 	sepol_class->s.value = value;
+
 	rc = symtab_init(&sepol_class->permissions, PERM_SYMTAB_SIZE);
 	if (rc != SEPOL_OK) {
 		goto class_to_policydb_out;
 	}
+
 	while (cil_perm != NULL) {
 		struct cil_perm *curr = cil_perm->data;
 		perm_datum_t *sepol_perm = cil_malloc(sizeof(*sepol_perm));
 		memset(sepol_perm, 0, sizeof(perm_datum_t));
+
 		key = cil_strdup(curr->datum.name);
-		sepol_perm->s.value = sepol_class->permissions.nprim + 1;
 		rc = hashtab_insert(sepol_class->permissions.table, key, sepol_perm);
 		if (rc != SEPOL_OK) {
 			goto class_to_policydb_out;
 		}
+		sepol_perm->s.value = sepol_class->permissions.nprim + 1;
+
 		sepol_class->permissions.nprim++;
 		cil_perm = cil_perm->next;
 	}
+
 	return SEPOL_OK;
 
 class_to_policydb_out:
@@ -121,47 +131,54 @@ class_to_policydb_out:
 
 int cil_type_to_policydb(policydb_t *pdb, struct cil_tree_node *node)
 {
-        int rc = SEPOL_ERR;
-        uint32_t value = 0;
-	char *key;
-        struct cil_type *cil_type = node->data;
-        type_datum_t *sepol_type = cil_malloc(sizeof(*sepol_type));
-        type_datum_init(sepol_type);
+	int rc = SEPOL_ERR;
+	uint32_t value = 0;
+	char *key = NULL;
+	struct cil_type *cil_type = node->data;
+	type_datum_t *sepol_type = cil_malloc(sizeof(*sepol_type));
+	type_datum_init(sepol_type);
+
+	sepol_type->primary = 1;
+	sepol_type->flavor = TYPE_TYPE;
+
 	key = cil_strdup(cil_type->datum.name);
-        sepol_type->primary = 1;
-        sepol_type->flavor = TYPE_TYPE;
-        rc = symtab_insert(pdb, SYM_TYPES, key, sepol_type, SCOPE_DECL, 0, &value);
-        if (rc != SEPOL_OK) {
-                goto type_to_binary_out;
-        }
-        sepol_type->s.value = value;
-        return SEPOL_OK;
+	rc = symtab_insert(pdb, SYM_TYPES, key, sepol_type, SCOPE_DECL, 0, &value);
+	if (rc != SEPOL_OK) {
+		goto type_to_binary_out;
+	}
+	sepol_type->s.value = value;
+
+	return SEPOL_OK;
 
 type_to_binary_out:
-        return rc;
+	return rc;
 }
 
 int policydb_type_ebitmap_init(policydb_t *pdb)
 {
-        int rc = SEPOL_ERR;
-        uint32_t i;
-        if (pdb->attr_type_map != NULL && pdb->type_attr_map != NULL) {
-                rc = SEPOL_OK;
+	int rc = SEPOL_ERR;
+	uint32_t i;
+
+	if (pdb->attr_type_map != NULL && pdb->type_attr_map != NULL) {
+		rc = SEPOL_OK;
 		goto type_ebitmap_out;
         }
-        pdb->attr_type_map = cil_malloc(pdb->p_types.nprim * sizeof(ebitmap_t));
-        pdb->type_attr_map = cil_malloc(pdb->p_types.nprim * sizeof(ebitmap_t));
-        for (i = 0; i < pdb->p_types.nprim; i++) {
-                ebitmap_init(&pdb->attr_type_map[i]);
-                ebitmap_init(&pdb->type_attr_map[i]);
-                if (ebitmap_set_bit(&pdb->type_attr_map[i], i, 1)) {
-                        goto type_ebitmap_out;
-                }
-        }
-        return SEPOL_OK;
+
+	pdb->attr_type_map = cil_malloc(pdb->p_types.nprim * sizeof(ebitmap_t));
+	pdb->type_attr_map = cil_malloc(pdb->p_types.nprim * sizeof(ebitmap_t));
+
+	for (i = 0; i < pdb->p_types.nprim; i++) {
+		ebitmap_init(&pdb->attr_type_map[i]);
+		ebitmap_init(&pdb->type_attr_map[i]);
+		if (ebitmap_set_bit(&pdb->type_attr_map[i], i, 1)) {
+			goto type_ebitmap_out;
+		}
+	}
+
+	return SEPOL_OK;
 
 type_ebitmap_out:
-        return rc;
+	return rc;
 }
 
 int __cil_node_to_policydb(policydb_t *pdb, struct cil_tree_node *node, int pass)
@@ -241,14 +258,17 @@ int cil_binary_create(const struct cil_db *db, policydb_t *pdb, const char *fnam
 		rc = SEPOL_ERR;
 		goto binary_create_out;
 	}
+
 	policy_file_init(&pf);
 	pf.type = PF_USE_STDIO;
 	pf.fp = binary;
+
 	rc = policydb_write(pdb, &pf);
 	if (rc != 0) {
 		fprintf(stderr, "Failed writing binary policy\n");
 		goto binary_create_out;
 	}
+
 	fclose(binary);
 
 	return SEPOL_OK;
